6bc4ed7d927316094dbfa6bb3e27f7e3b617c7fc
Harsithgajarla <gajarlaharshith7@gmail.com>
2026-08-12 22:17:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (67.19%) - LeetSync

---
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.c
@@ -0,0 +1,22 @@
+bool checkStraightLine(int** coordinates, int coordinatesSize, int* coordinatesColSize) 
+{
+    int flag=-1;
+
+    int x1=coordinates[0][0];
+    int y1=coordinates[0][1];
+    int x2=coordinates[1][0];
+    int y2=coordinates[1][1];
+
+    for(int i=0;i<coordinatesSize;i++)
+    {
+        int x3=coordinates[i][0];
+        int y3=coordinates[i][1];
+
+        if(((y2-y1)*(x3-x1))!=((y3-y1)*(x2-x1)))
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
